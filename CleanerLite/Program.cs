11dfe726e602bd929421ded56994c1caaec465b9
tiagossa1 <tssa.2000@tuta.io>
2021-08-08 18:45:35 +0100

    Add project files.

---
+++ CleanerLite/Program.cs
@@ -0,0 +1,96 @@
+﻿using FluentColorConsole;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CleanerLite
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (SystemFactory.GetOSPlatform() == OSPlatform.Windows)
+            {
+                if (!SystemFactory.IsCurrentProcessAdmin())
+                {
+                    ColorConsole.WithYellowText.WriteLine("This software is not running as administrator, so it might NOT delete specific files/folders.");
+                }
+
+                var builder = new ConfigurationBuilder().AddJsonFile("appSettings.json", true, true);
+
+                var config = builder.Build();
+                var paths = config.GetSection("Paths").GetChildren().Where(x => !string.IsNullOrWhiteSpace(x.Value)).Select(x => x.Value).ToList();
+
+                foreach (var path in paths)
+                {
+                    DeleteFilesAndFolders(path);
+                }
+            }
+            else
+            {
+                throw new Exception("This software only runs on Windows.");
+            }
+
+            ColorConsole.WithGreenText.WriteLine("Job done!");
+            Console.ReadKey();
+        }
+
+        private static void DeleteFilesAndFolders(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                string[] folders = Directory.GetDirectories(path);
+
+                DeleteFiles(files);
+                DeleteFolders(folders);
+            }
+        }
+
+        private static bool IsDirectoryEmpty(string folderPath)
+        {
+            if (!string.IsNullOrWhiteSpace(folderPath))
+            {
+                return !Directory.EnumerateFileSystemEntries(folderPath).Any();
+            }
+
+            return false;
+        }
+
+        private static void DeleteFolders(string[] folderPaths)
+        {
+            foreach (var folder in folderPaths)
+            {
+                try
+                {
+                    DeleteFilesAndFolders(folder);
+
+                    Directory.Delete(folder, true);
+                    ColorConsole.WithGreenText.WriteLine($"{folder} deleted.");
+                }
+                catch (Exception ex)
+                {
+                    ColorConsole.WithRedText.WriteLine($"Could not delete {folder}: {ex.Message}.");
+                }
+            }
+        }
+
+        private static void DeleteFiles(string[] files)
+        {
+            foreach (var file in files)
+            {
+                try
+                {
+                    File.Delete(file);
+                    ColorConsole.WithGreenText.WriteLine($"{file} deleted.");
+                }
+                catch (Exception ex)
+                {
+                    ColorConsole.WithRedText.WriteLine($"Could not delete {file}: {ex.Message}.");
+                }
+            }
+        }
+    }
+}
---
+++ CleanerLite/SystemFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace CleanerLite
+{
+    public static class SystemFactory
+    {
+        public static bool IsCurrentProcessAdmin()
+        {
+            using var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static OSPlatform GetOSPlatform()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return OSPlatform.Windows;
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return OSPlatform.Linux;
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return OSPlatform.OSX;
+            else
+                return OSPlatform.FreeBSD;
+        }
+    }
+}
